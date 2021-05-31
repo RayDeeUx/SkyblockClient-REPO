f6ce17b7bc60042e5f5675f61571c43fd3d3b4ab
Zordlan <fiercefalcon14@gmail.com>
2021-05-31 07:45:16 -0700

    remove useless newlines

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -62,9 +62,8 @@ export default class evaluate extends BotCommand {
 
             //im going to make something that disables eval embed in specific channels in the database later
             if (message.guild.id == `794610828317032458` && message.channel.id != `834878498941829181`) {
-                if (args.codetoeval.includes('message.delete')) {
-                    return
-                }
+                if (args.codetoeval.includes('message.delete')) {return}
+                
                 return message.react(`<:success:838816341007269908>`)
             }
 
