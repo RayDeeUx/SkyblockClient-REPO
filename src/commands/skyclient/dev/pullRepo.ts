bc076df7755fca6c152f4c5b07b9f9e19d077ebe
Zordlan <fiercefalcon14@gmail.com>
2021-06-12 09:44:58 -0700

    i dont know how to do this and both discord and google arent helping

---
+++ src/commands/skyclient/dev/pullRepo.ts
@@ -0,0 +1,35 @@
+import axios from "axios"
+import { exec } from "child_process";
+import { promisify } from "util";
+import { BotCommand } from '../../../extensions/BotCommand';
+import utils from "../../../functions/utils";
+
+const sh = promisify(exec)
+const fs = require('fs')
+
+
+export default class pullRepo extends BotCommand {
+    constructor() {
+        super('pullRepo', {
+            aliases: ['pullRepo'],
+            
+        })
+    }
+
+    async exec(message, args) {
+        const SkyClientGuilds = [
+            `780181693100982273`, //main server
+            `824680357936103497` //testing server
+        ]
+        const peopleWithPerms = [
+            '492488074442309642', //Zordlan
+            '435443705055543306', //nacrt
+            '378587857796726785' //Koxx12
+        ]
+        if (SkyClientGuilds.includes(message.guild.id) && peopleWithPerms.includes(message.author.id)) {
+            async function readFile(){ fs.readFile('SkyblockClient-REPO/files/botautoresponse.json')}
+            console.log(await readFile())
+        }
+        else { return }
+    }
+}
--- src/listeners/listeners.ts
+++ src/listeners/listeners.ts
@@ -1,6 +1,10 @@
 import chalk from 'chalk';
+import { exec } from 'child_process';
 import { Listener } from 'discord-akairo';
+import { promisify } from 'util';
 import { BotListener } from '../extensions/BotListener';
+
+const sh = promisify(exec)
 
 class ReadyListener extends BotListener {
     constructor() {
@@ -10,7 +14,10 @@ class ReadyListener extends BotListener {
         });
     }
 
-    exec() {
+    async exec() {
+        try{await sh('git clone https://github.com/nacrt/SkyblockClient-REPO')}
+        catch(err){if (err = `fatal: destination path 'SkyblockClient-REPO' already exists and is not an empty directory.`) {console.log(chalk.red('repo found'))}}
+        console.log(chalk.red('Cloned nacrt/SkyblockClient-REPO, to make sure that it\'s there!'))
         console.log(chalk.magenta(`Bot Online!`))
         console.log(`\n`)
         console.log(chalk.magentaBright(`---Bot Output---`))
