a0ae85e046c9ffac2f1ac41fccf1d6058d1cc4ea
Zordlan <fiercefalcon14@gmail.com>
2021-06-13 23:19:16 -0700

    add perms check

--- src/commands/staff/autoresponse/pullautoresponse.ts
+++ src/commands/staff/autoresponse/pullautoresponse.ts
@@ -16,12 +16,23 @@ export default class pullAutoresponse extends BotCommand {
         });
     }
     async exec(message) {
-        sh('cd SkyblockClient-REPO ; git pull')
-            .then(() => {
-                message.channel.send('pulled')
-            })
-            .catch(err => {
-                utils.errorhandling(err, message)
-            })
+        const coolPeople = [
+            '492488074442309642', //Zordlan
+            '435443705055543306', //nacrt
+            '464851580370419733', //micro
+            '378587857796726785', //koxx12
+        ]
+        if (coolPeople.includes(message.author.id)) {
+            sh('cd SkyblockClient-REPO ; git pull')
+                .then(() => {
+                    message.channel.send('pulled')
+                })
+                .catch(err => {
+                    utils.errorhandling(err, message)
+                })
+        }
+        else {
+            message.channel.send('hey so you dont have the perms to run this command, if you think you deserve them ping zord or make a pr')
+        }
     }
 }
