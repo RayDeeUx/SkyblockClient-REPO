5639bfc12afc2c6ec21a342320fb3b073504c95e
Zordlan <fiercefalcon14@gmail.com>
2021-06-25 00:04:49 -0700

    oh my god stop yelling at me about the faq command

---
+++ src/commands/skyclient/faq.ts
@@ -0,0 +1,56 @@
+import { MessageEmbed } from 'discord.js';
+import { BotCommand } from '../../extensions/BotCommand';
+import utils from '../../functions/utils';
+
+export default class faq extends BotCommand {
+    constructor() {
+        super('faq', {
+            aliases: ['faq'],
+            args: [
+                { id: "question", type: "string" }
+            ],
+
+            slash: true,
+            slashGuilds: utils.slashGuilds,
+            slashOptions: [
+                {
+                    name: 'question',
+                    description: 'ID of the issue you want to see',
+                    type: 'STRING',
+                    choices: [
+                        {
+                            name: 'Are any of the mods bannable?',
+                            value: 'anymodsbannable'
+                        },
+                        {
+                            name: "Where's the download?",
+                            value: 'wheredownload'
+                        }
+                    ]
+                }
+            ],
+            description: 'A bunch of the frequently asked questions about SkyClien\'t!'
+        });
+    }
+
+    async exec(message, args) {
+        if (utils.SkyClientGuilds.includes(message.guild.id)) {
+            const embed = new MessageEmbed()
+
+            if (!args.question) { return message.reply('<#780185114352549919>') }
+
+            if (args.question.toLowerCase() == 'anymodsbannable') {
+                embed.setTitle('Are any of the mods bannable?')
+                embed.setDescription('No.')
+            }
+
+            if (args.question.toLowerCase() == 'wheredownload') {
+                embed.setTitle("Where's the download?")
+                embed.setDescription(`<#780940408175853609> if on Windows 10 (untested on 11)
+                <#782998702289059860> if on literally anything else (you will need [java 8](https://adoptopenjdk.net/?variant=openjdk8&jvmVariant=hotspot))`)
+            }
+
+            message.reply({ embeds: [embed] })
+        }
+    }
+}
