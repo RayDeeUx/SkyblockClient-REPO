8f0e7746f8e882523824c6e44ec9b9bcc286dd67
Zordlan <fiercefalcon14@gmail.com>
2021-06-25 20:51:51 -0700

    autoban for fake steam links

---
+++ src/listeners/thisisaminecraftdiscordnotacsgotradingdiscord.ts
@@ -0,0 +1,35 @@
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import { BotListener } from '../extensions/BotListener';
+import fs from 'fs';
+
+class thisIsAMinecraftModDiscordNotACSGOTradingDiscord extends BotListener {
+    constructor() {
+        super('thisIsAMinecraftModDiscordNotACSGOTradingDiscord', {
+            emitter: 'client',
+            event: 'message'
+        });
+    }
+
+    async exec(message) {
+        try {
+            const fsJson = fs.readFileSync('src/listeners/fakeSteamcommunityLinks.json', 'utf8')
+            let ohMyFuckingGodThisIsADiscordForMinecraftNotForCSGOTradingOrScammingOfAnyKind = JSON.parse(fsJson)
+
+            ohMyFuckingGodThisIsADiscordForMinecraftNotForCSGOTradingOrScammingOfAnyKind.forEach(async fakeSteamLink => {
+                if (message.content.includes(fakeSteamLink)) {
+                    if (message.member.bannable) {
+                        message.member.ban({ days: 1, reason: 'Sending a fake Steam link' })
+                        message.delete()
+                    }
+                    else {
+                        message.delete()
+                    }
+                }
+            })
+        }
+        catch (err) { return }
+    }
+}
+
+module.exports = thisIsAMinecraftModDiscordNotACSGOTradingDiscord;
