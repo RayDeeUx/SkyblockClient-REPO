7fab51db3679833b02b187e8f7e1818020d05bf3
Zordlan <fiercefalcon14@gmail.com>
2021-06-28 18:27:38 -0700

    more things

--- src/commands/owner/say.ts
+++ src/commands/owner/say.ts
@@ -5,6 +5,7 @@ export default class say extends BotCommand {
     constructor() {
         super('say', {
             aliases: ['say'],
+            args: [{ id: 'thingtosay', type: 'string', match: 'restContent' }],
             slash: true,
             description: 'Sends a message of your choice!',
             slashGuilds: ['824680357936103497', '780181693100982273'],
@@ -14,7 +15,7 @@ export default class say extends BotCommand {
             ownerOnly: true
         })
     }
-    async execSlash(message, slashOptions) {
+    async exec(message, slashOptions) {
         message.channel.send(slashOptions.thingtosay).then(msg => {
             const sentEmbed = new MessageEmbed()
                 .setTitle('Message sent!')
