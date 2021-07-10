f946e6e1c83d190d0a9dcd1a2a5c3854aa6db233
Zordlan <fiercefalcon14@gmail.com>
2021-07-10 09:18:17 -0700

    fuck you git

---
+++ src/listeners/updateListener.ts
@@ -0,0 +1,27 @@
+import { BotListener } from '../extensions/BotListener';
+
+class autoUpdateListener extends BotListener {
+    constructor() {
+        super('autoUpdateListener', {
+            emitter: 'client',
+            event: 'message'
+        });
+    }
+
+    async exec(message) {
+        if (message.channel.id == '829044767626493994') {
+
+            const regex = /\[SkyClientBot-TEMP:master] [1-9][1-9]? new commits?/g
+            if (message.embeds[0] && regex.test(message.embeds[0].title)) {
+                const gitPullCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'gitPull'))
+
+                gitPullCommand.exec(message, '').then(() => {
+                    const reloadCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'reload'))
+                    reloadCommand.exec(message, '')
+                })
+            }
+        }
+    }
+}
+
+module.exports = autoUpdateListener;
