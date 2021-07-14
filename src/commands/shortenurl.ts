b1c7b452d24eb45835cb6c3d62747c84b9473187
Zordlan <fiercefalcon14@gmail.com>
2021-07-14 10:29:31 -0700

    shortenurl command

---
+++ src/commands/shortenurl.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import language from "../constants/language";
+import { BotCommand } from '../extensions/BotCommand';
+import utils from "../functions/utils";
+
+export default class shortenurl extends BotCommand {
+    constructor() {
+        super('shortenurl', {
+            aliases: ['shortenurl'],
+            args: [{ id: 'url', type: 'string', match: 'restContent' }],
+
+            slash: true,
+            description: 'Shortens a URL',
+            slashGuilds: utils.slashGuilds,
+            slashOptions: [
+                {
+                    name: 'url',
+                    description: 'the URL you want shortened',
+                    type: 'STRING',
+                    required: true
+                }
+            ],
+        })
+    }
+
+    async exec(message, args) {
+        const shortenedurl = (await axios.get(`https://api.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com/a?url=${args.url}`)).data
+
+        if (message.type == 'REPLY') {
+            if (message.channel.type == 'text') {
+                const repliedMessage = await message.channel.messages.fetch(message.reference.messageID)
+                repliedMessage.reply(shortenedurl)
+            }
+        }
+        else {
+            message.reply(shortenedurl)
+        }
+    }
+}
