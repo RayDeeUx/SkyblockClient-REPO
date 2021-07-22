2877222fa52b0451b833aa35ab356aeaaa7bbcae
Zordlan <fiercefalcon14@gmail.com>
2021-07-21 22:24:48 -0700

    it would be a good idea to put all of the code in the same if statement

--- src/listeners/thisisaminecraftdiscordnotacsgotradingdiscord.ts
+++ src/listeners/thisisaminecraftdiscordnotacsgotradingdiscord.ts
@@ -37,15 +37,15 @@ class thisIsAMinecraftModDiscordNotACSGOTradingDiscord extends BotListener {
                             message.guild.channels.cache.get('796895966414110751').send(`${message.author.tag} sent a scam link`)
                             return message.channel.send(`hey yeah you shouldn't send those ${message.author}`)
                         }
-                    }
 
-                    if (message.member.bannable) {
-                        console.log('banning')
-                        try { await message.author.send('Hey, did you know that we ban for scamming?') }
-                        catch (err) { return }
-                        message.member.ban({ reason: 'Sending a scam link' })
-                        message.delete()
-                        message.guild.channels.cache.get('796895966414110751').send(`${message.author.tag} has been banned for steam scam link`)
+                        if (message.member.bannable) {
+                            console.log('banning')
+                            try { await message.author.send('Hey, did you know that we ban for scamming?') }
+                            catch (err) { return }
+                            message.member.ban({ reason: 'Sending a scam link' })
+                            message.delete()
+                            message.guild.channels.cache.get('796895966414110751').send(`${message.author.tag} has been banned for steam scam link`)
+                        }
                     }
                 }
             })
