de067c3b9bfdbf9c8790473cf6d26bdcc492e28c
Zordlan <fiercefalcon14@gmail.com>
2021-07-23 14:12:45 -0700

    change command getting locations in autoupdate

--- src/listeners/updateListener.ts
+++ src/listeners/updateListener.ts
@@ -10,13 +10,14 @@ class autoUpdateListener extends BotListener {
 
     async exec(message) {
         if (message.channel.id == '848931262248517732') {
+            const gitPullCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'gitPull'))
+            const reloadCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'reload'))
+
 
             const regex = /\[SkyClientBot-TEMP:master\] [0-9]+ new commits?/g
             if (message.embeds[0] && regex.test(message.embeds[0].title)) {
-                const gitPullCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'gitPull'))
 
                 gitPullCommand.exec(message, '').then(() => {
-                    const reloadCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'reload'))
                     reloadCommand.exec(message, '')
                 })
             }
