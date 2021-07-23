28217d4683e90fb883e4c688d6c47ad0c5337731
Zordlan <fiercefalcon14@gmail.com>
2021-07-23 14:10:13 -0700

    new regex maybe?

--- src/listeners/updateListener.ts
+++ src/listeners/updateListener.ts
@@ -11,7 +11,7 @@ class autoUpdateListener extends BotListener {
     async exec(message) {
         if (message.channel.id == '848931262248517732') {
 
-            const regex = /\[SkyClientBot-TEMP:master] [1-9][0-9]? new commits?/g
+            const regex = /\[SkyClientBot-TEMP:master] [0-9]+ new commits?/g
             if (message.embeds[0] && regex.test(message.embeds[0].title)) {
                 const gitPullCommand = this.client.commandHandler.modules.find(cmd => (cmd.id == 'gitPull'))
 
