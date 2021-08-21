3de2b5790bc34d713b4126044f78f2f431bbadb0
Zordlan <fiercefalcon14@gmail.com>
2021-08-21 16:37:10 -0700

    update invite command

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -6,6 +6,7 @@ export default class invite extends BotCommand {
     constructor() {
         super('invite', {
             aliases: ['invite'],
+            args:[{id:'ephemeral', type:'boolean', default:'false'}],
 
             slash: true,
             description: '"Invite the bot"',
@@ -21,6 +22,10 @@ export default class invite extends BotCommand {
     }
 
     async exec(message, args) {
+        if (message.interaction) {
+            return await message.reply({content:'[https://discord.com/api/oauth2/authorize?client_id=826247426922250280&permissions=8&scope=bot%20applications.commands](https://www.youtube.com/watch?v=dQw4w9WgXcQ)', ephemeral:args.ephemeral})
+        }
+
         const inviteEmbed = new MessageEmbed()
             .setTitle('Invite me to your server!')
             .setDescription('[A few things you should know](https://genius.com/Rick-astley-never-gonna-give-you-up-lyrics)')
