e30e61be00f03ca80ce26658856d70533b4bd8bf
Zordlan <fiercefalcon14@gmail.com>
2021-08-21 15:23:06 -0700

    fix pronouns command

--- src/commands/pronouns.ts
+++ src/commands/pronouns.ts
@@ -6,23 +6,22 @@ export default class pronouns extends BotCommand {
     constructor() {
         super('pronouns', {
             aliases: ['pronouns'],
-            args: [{ id: 'person', type: 'user', match: 'rest' }],
+            args: [{ id: 'user', type: 'string', match: 'rest' }],
 
             description: 'Shows the pronouns of a user, if they have them set on https://pronoundb.org',
     
-            slashOptions:[{name:'person',type:'STRING',description:'the person you want to know the pronouns of'}],
+            slashOptions:[{name:'person',type:'USER',description:'the person you want to know the pronouns of'}],
             slash: true,
             slashGuilds: utils.slashGuilds
         })
     }
     async exec(message, args) {
-        console.log(args.person)
-        const person = await utils.fetchUser(args.person || message.author.id || message.user.id)
+        const person = await utils.fetchUser(args.user ?? args.person ?? message.author.id)
 
         const pronouns = await utils.getPronouns(person, 'details')
         const pronounsEmbed = new MessageEmbed()
 
-        if (args.person.id == message.author.id) { pronounsEmbed.setTitle('Your pronouns') }
+        if (person.id == message.author.id) { pronounsEmbed.setTitle('Your pronouns') }
         else { pronounsEmbed.setTitle(`${person.username}'s pronouns`) }
 
         pronounsEmbed.setDescription(pronouns)
