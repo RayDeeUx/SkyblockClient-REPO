82e4671e22f9697b32d9ec318c0c21506a010321
Lisena <lisenawastaken@gmail.com>
2021-09-14 15:09:00 -0700

    autoquote now won't ping everyone

--- src/functions/commandManager.ts
+++ src/functions/commandManager.ts
@@ -1,6 +1,7 @@
 import { Message, Snowflake } from 'discord.js'
 
 const bypassRoles = [
+	//skyclient
 	'780182606628782100',
 	'780183853628260352',
 	'832754819588292679',
@@ -11,41 +12,70 @@ const bypassRoles = [
 	'844275169010647050',
 	'799020944487612428',
 	'780202942321655858',
+
+	//lis' boat
+	'880717671967105044',
+	'880705826627649566',
 ] as unknown as Snowflake
 
 function userCanUseCommand(message: Message) {
-	if (message.guild.id != '780181693100982273') {
-		return true
-	}
+	// if (message.guild.id != '780181693100982273') {
+	// 	return true
+	// }
+	if (message.guildId === '780181693100982273') {
+		let userHasBypassRole = false
 
-	let userHasBypassRole = false
+		const allowedChannels = [
+			'796546551878516766', //bot commands
+		]
+		let channelIsAllowed = false
 
-	const allowedChannels = [
-		'796546551878516766', //bot commands
-	]
-	let channelIsAllowed = false
+		const guildID = '780181693100982273'
 
-	const guildID = '780181693100982273'
-
-	message.member.roles.cache.forEach((role) => {
-		if (bypassRoles.includes(role.id)) {
-			return (userHasBypassRole = true)
+		message.member.roles.cache.forEach((role) => {
+			if (bypassRoles.includes(role.id)) {
+				return (userHasBypassRole = true)
+			}
+		})
+		allowedChannels.forEach((channel) => {
+			if (message.channel.id === channel) {
+				return (channelIsAllowed = true)
+			}
+		})
+		if (userHasBypassRole) {
+			return true
 		}
-	})
-	allowedChannels.forEach((channel) => {
-		if (message.channel.id == channel) {
-			return (channelIsAllowed = true)
+		if (channelIsAllowed) {
+			return true
 		}
-	})
-	//@ts-ignore
-	if (userHasBypassRole == true) {
-		return true
-	}
-	//@ts-ignore
-	if (channelIsAllowed == true) {
-		return true
-	}
-	return false
+		return false
+	} else if (message.guildId === '880637463838724166') {
+		let userHasBypassRole = false
+
+		const allowedChannels = [
+			'880721465530544149', //bot commands,
+		]
+		let channelIsAllowed = false
+
+		const guildID = '780181693100982273'
+
+		message.member.roles.cache.forEach((role) => {
+			if (bypassRoles.includes(role.id)) {
+				return (userHasBypassRole = true)
+			}
+		})
+		allowedChannels.forEach((channel) => {
+			if (message.channel.id === channel) {
+				return (channelIsAllowed = true)
+			}
+		})
+		if (userHasBypassRole) return true
+
+		if (channelIsAllowed) return true
+
+		if (message.guild.ownerId === message.author.id) return true
+		return false
+	} else return true
 }
 
 export = {
--- src/listeners/autoquote.ts
+++ src/listeners/autoquote.ts
@@ -1,5 +1,6 @@
 import { BotListener } from '../extensions/BotListener'
 import { Guild, Message, TextChannel } from 'discord.js'
+import commandManager from '../functions/commandManager'
 
 module.exports = class autoquote extends BotListener {
 	constructor() {
@@ -12,16 +13,19 @@ module.exports = class autoquote extends BotListener {
 	async exec(message: Message) {
 		try {
 			if (message.author.bot) return
+			if (!commandManager.userCanUseCommand(message)) return
 
-			const urlRegex = /https:\/\/(?:\w+\.)?discord\.com\/channels\/(\d{18})\/(\d{18})\/(\d{18})/g
+			const urlRegex = /https:\/\/(?:\w+\.)?discord(app)?\.com\/channels\/(\d{18})\/(\d{18})\/(\d{18})/g
 			const matches = [...message.content.matchAll(urlRegex)]
 
 			if (matches.length === 0) return
 
 			const messageArray = matches
+			const messageSet = new Set()
+			messageArray.forEach((m) => messageSet.add(m[0]))
 
-			messageArray.forEach(async (msgArray) => {
-				const mArray = msgArray[0].split('/')
+			messageSet.forEach(async (msgLink: string) => {
+				const mArray = msgLink.split('/')
 				const guildID = mArray[mArray.length - 3]
 				const channelID = mArray[mArray.length - 2]
 				const messageID = mArray[mArray.length - 1]
@@ -40,37 +44,84 @@ module.exports = class autoquote extends BotListener {
 				const msg = await channel.messages.fetch(messageID)
 				if (msg === undefined) return
 
-				if (message.channel.type === 'GUILD_TEXT') {
-					const webhooks = await (message.channel as TextChannel).fetchWebhooks()
-					const foundWebhook = webhooks.find((w) => w.name == `Rain Quoting - ${(message.channel as TextChannel).name}` && w.owner?.id === this.client.user?.id)
-					let webhook
-					if (foundWebhook === undefined) {
-						webhook = await (message.channel as TextChannel).createWebhook(`Rain Quoting - ${(message.channel as TextChannel).name}`, { avatar: this.client.user?.displayAvatarURL() })
-					} else if (foundWebhook.owner?.id != this.client.user?.id) {
-						webhook = await (message.channel as TextChannel).createWebhook(`Rain Quoting - ${(message.channel as TextChannel).name}`, { avatar: this.client.user?.displayAvatarURL() })
-					} else {
-						webhook = foundWebhook
-					}
+				// console.log(`${msg.type}: ${typeof msg.type}`)
+				// if (msg.type != 'DEFAULT') return
+				// console.log('this should not happen')
 
-					if (msg.content)
-						await webhook.send({ content: msg.content, files: msg.attachments.toJSON(), embeds: msg.embeds, username: `${msg.author.tag}`, avatarURL: msg.author.displayAvatarURL() })
-					else await webhook.send({ files: msg.attachments.toJSON(), embeds: msg.embeds, username: `${msg.author.tag}`, avatarURL: msg.author.displayAvatarURL() })
-				}
-				else if (message.channel.type === 'GUILD_PUBLIC_THREAD' || message.channel.type === 'GUILD_PRIVATE_THREAD') {
-					const webhooks = await (message.channel.parent as TextChannel).fetchWebhooks()
-					const foundWebhook = webhooks.find((w) => w.name == `Rain Quoting - ${(message.channel as TextChannel).name}` && w.owner?.id === this.client.user?.id)
-					let webhook
-					if (foundWebhook === undefined) {
-						webhook = await (message.channel.parent as TextChannel).createWebhook(`Rain Quoting - ${(message.channel.parent as TextChannel).name}`, { avatar: this.client.user?.displayAvatarURL() })
-					} else if (foundWebhook.owner?.id != this.client.user?.id) {
-						webhook = await (message.channel.parent as TextChannel).createWebhook(`Rain Quoting - ${(message.channel.parent as TextChannel).name}`, { avatar: this.client.user?.displayAvatarURL() })
-					} else {
-						webhook = foundWebhook
-					}
+				if (msg.type === 'DEFAULT') {
+					if (message.channel.type === 'GUILD_TEXT') {
+						let author
+						if (msg.webhookId) author = msg.author.tag.substring(0, msg.author.tag.length - 5)
+						else author = msg.author.tag
 
-					if (msg.content)
-						await webhook.send({ content: msg.content, files: msg.attachments.toJSON(), embeds: msg.embeds, username: `${msg.author.tag}`, avatarURL: msg.author.displayAvatarURL(), threadId: message.channelId })
-					else await webhook.send({ files: msg.attachments.toJSON(), embeds: msg.embeds, username: `${msg.author.tag}`, avatarURL: msg.author.displayAvatarURL(), threadId: message.channelId })
+						const webhooks = await (message.channel as TextChannel).fetchWebhooks()
+						const foundWebhook = webhooks.find((w) => w.name == `Rain Quoting - ${(message.channel as TextChannel).name}` && w.owner?.id === this.client.user?.id)
+						let webhook
+						if (foundWebhook === undefined) {
+							webhook = await (message.channel as TextChannel).createWebhook(`Rain Quoting - ${(message.channel as TextChannel).name}`, { avatar: this.client.user?.displayAvatarURL() })
+						} else if (foundWebhook.owner?.id != this.client.user?.id) {
+							webhook = await (message.channel as TextChannel).createWebhook(`Rain Quoting - ${(message.channel as TextChannel).name}`, { avatar: this.client.user?.displayAvatarURL() })
+						} else {
+							webhook = foundWebhook
+						}
+
+						if (msg.content)
+							await webhook.send({
+								content: msg.content,
+								files: msg.attachments.toJSON(),
+								embeds: msg.embeds,
+								username: `${author}`,
+								allowedMentions: { parse: [] },
+								avatarURL: msg.author.displayAvatarURL(),
+							})
+						else
+							await webhook.send({
+								files: msg.attachments.toJSON(),
+								embeds: msg.embeds,
+								username: `${author}`,
+								allowedMentions: [],
+								avatarURL: msg.author.displayAvatarURL(),
+							})
+					} else if (message.channel.type === 'GUILD_PUBLIC_THREAD' || message.channel.type === 'GUILD_PRIVATE_THREAD') {
+						let author
+						if (msg.webhookId) author = msg.author.tag.substring(0, msg.author.tag.length - 5)
+						else author = msg.author.tag
+
+						const webhooks = await (message.channel.parent as TextChannel).fetchWebhooks()
+						const foundWebhook = webhooks.find((w) => w.name == `Rain Quoting - ${(message.channel as TextChannel).name}` && w.owner?.id === this.client.user?.id)
+						let webhook
+						if (foundWebhook === undefined) {
+							webhook = await (message.channel.parent as TextChannel).createWebhook(`Rain Quoting - ${(message.channel.parent as TextChannel).name}`, {
+								avatar: this.client.user?.displayAvatarURL(),
+							})
+						} else if (foundWebhook.owner?.id != this.client.user?.id) {
+							webhook = await (message.channel.parent as TextChannel).createWebhook(`Rain Quoting - ${(message.channel.parent as TextChannel).name}`, {
+								avatar: this.client.user?.displayAvatarURL(),
+							})
+						} else {
+							webhook = foundWebhook
+						}
+
+						if (msg.content)
+							await webhook.send({
+								content: msg.content,
+								files: msg.attachments.toJSON(),
+								embeds: msg.embeds,
+								username: `${author}`,
+								avatarURL: msg.author.displayAvatarURL(),
+								allowedMentions: [],
+								threadId: message.channelId,
+							})
+						else
+							await webhook.send({
+								files: msg.attachments.toJSON(),
+								embeds: msg.embeds,
+								username: `${author}`,
+								avatarURL: msg.author.displayAvatarURL(),
+								allowedMentions: [],
+								threadId: message.channelId,
+							})
+					}
 				}
 			})
 		} catch (err) {
