fc25d94648dfea05a0b41abf5278b391da2c0fc6
Lisena <69115669+Lisenaaaa@users.noreply.github.com>
2021-10-30 17:05:34 -0700

    make it check for message.author in the command error listener

--- src/listeners/error/commandError.ts
+++ src/listeners/error/commandError.ts
@@ -8,7 +8,7 @@ export default class commandErrorListener extends BotListener {
 		})
 	}
 	async exec(error, message) {
-		if (this.client.ownerID.includes(message.author.id) || this.client.ownerID.includes(message.user.id)) {
+		if (message.author && this.client.ownerID.includes(message.author.id) || this.client.ownerID.includes(message.user.id)) {
 			await message.reply({ content: `An error occured!\n\`\`\`js\n${error.stack}\`\`\``, ephemeral: true })
 		} else {
 			await message.reply({ embeds: [this.client.error(error, ' command', message)] })
