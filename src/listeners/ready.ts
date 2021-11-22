cf04a2ef726e83b4f5110d6c813e8398c335251c
Lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-22 14:51:25 -0800

    akairo won't install this might not work i hate windows so much

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -10,6 +10,7 @@ class ReadyListener extends BotListener {
 		super('ready', {
 			emitter: 'client',
 			event: 'ready',
+			once: true
 		})
 	}
 
@@ -19,7 +20,7 @@ class ReadyListener extends BotListener {
 		console.log(`\n`)
 		console.log(chalk.magentaBright(`---Bot Output---`))
 
-		this.client.user.setActivity('Raine create me', { type: 'WATCHING' })
+		this.client.user.setActivity('Katy create me', { type: 'WATCHING' })
 
 		//const statusJson = JSON.parse(fs.readFileSync("status.json", "ascii"))
 
