2ab6f2588161934bc7f4f22d9b2886716f698118
Lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-24 07:26:58 -0800

    remove optifine block

--- src/commands/skyclient/mod/modinfo.ts
+++ src/commands/skyclient/mod/modinfo.ts
@@ -39,7 +39,7 @@ export default class modInfo extends BotCommand {
 
 		const modEmbed = new MessageEmbed().setTitle(mod.display).setDescription(mod.description)
 		if (mod.command) modEmbed.addField('Command', mod.command)
-		if (mod.url && mod.id != 'optifine') modEmbed.addField('Direct Download', `[${mod.file}](${mod.url})`)
+		if (mod.url) modEmbed.addField('Direct Download', `[${mod.file}](${mod.url})`)
 		else if (!mod.url && mod.id != 'optifine')
 			modEmbed.addField('Direct Download', `[${mod.file}](https://github.com/nacrt/SkyblockClient-REPO/blob/main/files/mods/${encodeURIComponent(mod.file)}?raw=true)`)
 
