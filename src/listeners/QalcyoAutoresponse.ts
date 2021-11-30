380d2f4b5e80259392e4bbd45a9d2aa048bc095f
Lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2021-11-30 10:35:16 -0800

    remove debug shit and fix it

--- src/listeners/QalcyoAutoresponse.ts
+++ src/listeners/QalcyoAutoresponse.ts
@@ -11,36 +11,33 @@ export default class QalcyoAutoresponse extends BotListener {
 
 	async exec(message) {
 		if (!message.guild) return
-		if (message.guild.id != '884212442664697937' || message.guild.id != '914828318409388102') return
-		if (message.channel.parent.id != '901802497398693918' || message.channel.parent.id != '914830235382480936') return
-		if (message.channel.type != 'GUILD_TEXT' && message.channel.type != 'GUILD_PUBLIC_THREAD') {
-			return
-		}
-		if (message.content.startsWith('-') || message.content.startsWith('b!') || message.content.startsWith('t!') || message.content.startsWith('$') || message.content.startsWith('```')) return
-		if (message.author.bot != false) {
-			return
-		}
+		if (message.guild.id == '884212442664697937' || message.guild.id == '914828318409388102') {
+			if (message.channel.parent.id == '901802497398693918' || message.channel.parent.id == '914830235382480936') {
+				if (message.channel.type != 'GUILD_TEXT' && message.channel.type != 'GUILD_PUBLIC_THREAD') {
+					return
+				}
+				if (message.content.startsWith('-') || message.content.startsWith('b!') || message.content.startsWith('t!') || message.content.startsWith('$') || message.content.startsWith('```'))
+					return
+				if (message.author.bot != false) {
+					return
+				}
 
-		let noAutorespond = false
+				let response: any
+				this.client.QalcyoAutoresponse.forEach(async (trigger) => {
+					const triggers = trigger.triggers
+					const content = message.content.toLowerCase()
 
-		if (message.member.roles.cache.has('852016624605462589')) {
-			return (noAutorespond = true)
-		}
+					let contains = this.recursiveSearch(content, triggers, 0)
+					if (contains) {
+						response = trigger.response
 
-		let response: any
-		this.client.QalcyoAutoresponse.forEach(async (trigger) => {
-			const triggers = trigger.triggers
-			const content = message.content.toLowerCase()
+						const row = new MessageActionRow().addComponents(new MessageButton().setLabel('Delete').setStyle('DANGER').setCustomId(`arDelMsg|${message.author.id}`))
 
-			let contains = this.recursiveSearch(content, triggers, 0)
-			if (contains && noAutorespond == false) {
-				response = trigger.response
-
-				const row = new MessageActionRow().addComponents(new MessageButton().setLabel('Delete').setStyle('DANGER').setCustomId(`arDelMsg|${message.author.id}`))
-
-				await message.reply({ content: response, components: [row] })
+						await message.reply({ content: response, components: [row] })
+					}
+				})
 			}
-		})
+		}
 	}
 
 	recursiveSearch(cutContent: string, triggers: Array<Array<string>>, index: number): boolean {
