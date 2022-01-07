9af44e757b3f2d148f45939f5e9f59f3229d2686
lisenaaaa <69115669+Lisenaaaa@users.noreply.github.com>
2022-01-07 10:17:57 -0800

    add error handling

--- src/functions/msgutils.ts
+++ src/functions/msgutils.ts
@@ -1,43 +1,48 @@
 import { ReplyMessageOptions, Message } from 'discord.js'
+import client from '..'
 import commandManager from './commandManager'
 
 async function reply(message: Message, data: ReplyMessageOptions, forceEphemeral?: boolean) {
-	let content
-	if (!data.content && !message.interaction) content = { content: 'Please use my slashcommands! Message commands will stop working when the bot fully releases.', ...data }
-	else content = data
+	try {
+		let content
+		if (!data.content && !message.interaction) content = { content: 'Please use my slashcommands! Message commands will stop working when the bot fully releases.', ...data }
+		else content = data
 
-	if (forceEphemeral && message.interaction && commandManager.userCanUseCommand(message)) {
-		let ephemeralReplyContent = {
-			...content,
-			ephemeral: true,
-		}
-		return await message.reply(ephemeralReplyContent)
-	}
-
-	if (commandManager.userCanUseCommand(message)) {
-		if (message.type == 'REPLY') {
-			if (message.channel.type == 'GUILD_TEXT') {
-				const repliedMessage = await message.channel.messages.fetch(message.reference.messageId)
-
-				let coolReplyContent = {
-					...content,
-					allowedMentions: { repliedUser: true },
-				}
-				return await repliedMessage.reply(coolReplyContent)
+		if (forceEphemeral && message.interaction && commandManager.userCanUseCommand(message)) {
+			let ephemeralReplyContent = {
+				...content,
+				ephemeral: true,
 			}
-		} else {
-			return await message.reply(content)
+			return await message.reply(ephemeralReplyContent)
 		}
-	}
-	if (!commandManager.userCanUseCommand(message) && message.interaction) {
-		let ephemeralReplyContent = {
-			...content,
-			ephemeral: true,
-		}
-		return await message.reply(ephemeralReplyContent)
-	}
 
-	if (!commandManager.userCanUseCommand(message) && !message.interaction) return await message.reply('<#796546551878516766> or use as a slashcommand')
+		if (commandManager.userCanUseCommand(message)) {
+			if (message.type == 'REPLY') {
+				if (message.channel.type == 'GUILD_TEXT') {
+					const repliedMessage = await message.channel.messages.fetch(message.reference.messageId)
+
+					let coolReplyContent = {
+						...content,
+						allowedMentions: { repliedUser: true },
+					}
+					return await repliedMessage.reply(coolReplyContent)
+				}
+			} else {
+				return await message.reply(content)
+			}
+		}
+		if (!commandManager.userCanUseCommand(message) && message.interaction) {
+			let ephemeralReplyContent = {
+				...content,
+				ephemeral: true,
+			}
+			return await message.reply(ephemeralReplyContent)
+		}
+
+		if (!commandManager.userCanUseCommand(message) && !message.interaction) return await message.reply('<#796546551878516766> or use as a slashcommand')
+	} catch (err) {
+		await message.reply({ embeds: [await client.error(err)] })
+	}
 }
 
 export = {
